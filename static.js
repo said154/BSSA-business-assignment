34e0e9e191d5defd4c839ae77b266682286bb495
noobpro87 <39122989+noobpro87@users.noreply.github.com>
2018-09-07 11:35:17 +0200

    Update static.js

--- static.js
+++ static.js
@@ -1,32 +1,11 @@
-express.static(business website, [options])
+const express = require('express')
+const app = express()
 
-app.use(express.static('business website'))
+app.get('/', (req, res) => res.send('Hi'))
 
-http://localhost:3000/businesswebsite
-http://localhost:3000/chatbot.css
-http://localhost:3000/chatbot.html
-http://localhost:3000/chatbot.js
-http://localhost:3000/contact.html
-http://localhost:3000/images/createserver.js
-http://localhost:3000/home.html
-http://localhost:3000/news.html
-http://localhost:3000/nodejsserver.js
-http://localhost:3000/OverOns.html
-http://localhost:3000/servernode.js
-http://localhost:3000/slider.html
-http://localhost:3000/slider.js
-http://localhost:3000/static.js
-http://localhost:3000/
 
-app.use(express.static('business website'))
-//app.use(express.static(''))
 
-//app.use('/static', express.static('public'))
-//
-// http://localhost:3000/static/
-// http://localhost:3000/static/
-// http://localhost:3000/static/
-// http://localhost:3000/static/
-// http://localhost:3000/static/
-//
-// app.use('/static', express.static(path.join(__dirname, 'public')))
+app.listen(3000, () => console.log('Hi'))
+
+app.use(express.static(''))
+
